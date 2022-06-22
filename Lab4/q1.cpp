755feba5d3d9922ebb89bf1b0671bc86f8be3fec
Rohan69420 <rohan13maharjan@gmail.com>
2022-06-22 09:54:31 +0545

    q2 messing up with my brain

---
+++ Lab4/q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class ttime{
+    public:
+    ttime(){
+        hour=0,minute=0,second=0;
+        t12=false;
+    }
+    ttime(int hr, int min,int sec){
+        hour=hr;
+        minute=min;
+        second=sec;
+    }
+    void addttime(ttime &t){
+        hour+=t.hour;
+        minute+=t.minute;
+        second+=t.second;
+    }
+    void displayFormat(){
+        cout<<"Do you want 12 hr format?(y/n)"<<endl;
+        cin>>ch;
+        if (ch=='y')
+        t12=true;
+        if (t12){
+            cout<<"ttime: "<<hour%13<<":"<<minute<<":"<<second;
+        }
+        else{
+            cout<<"ttime: "<<hour<<":"<<minute<<":"<<second;
+        }
+
+    }
+    
+    private:
+    bool t12;
+    char ch;
+    int hour,minute, second;
+};
+int main(){
+    ttime t;
+    ttime t1(2,10,30);
+    ttime t2(3,30,20);
+    t2.addttime(t1);
+    t2.displayFormat();
+}

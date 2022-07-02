cd87a2a66d54e6b45d5bdeb84ce784caf54c2945
RohanM <rohan13maharjan@gmail.com>
2022-07-02 21:09:27 +0545

    Lab 4 all done

--- Lab4/q6.cpp
+++ Lab4/q6.cpp
@@ -8,9 +8,20 @@ class statDemo{
     static void inc(){
         count++;
     }
+    void display(){
+        std::cout<<count<<std::endl;
+    }
+
 
 };
 int statDemo::count=0;
 int main(){
-    classes
+    statDemo s1,s2,s3;
+    s1.display();
+    s1.inc();
+    s2.display();
+    s2.inc();
+    s3.display();
+    s3.inc();
+    
 }
